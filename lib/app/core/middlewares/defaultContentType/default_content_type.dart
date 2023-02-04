881e5ce32ccf8f392018e04d1a7c9ed2b5d4a542
deyvissoneduardo <deyvissoneduardo22@gmail.com>
2023-02-04 17:54:22 -0300

    middlewares

---
+++ lib/app/core/middlewares/defaultContentType/default_content_type.dart
@@ -0,0 +1,12 @@
+import 'package:shelf/shelf.dart';
+import 'package:stores_api/app/core/middlewares/middlewares.dart';
+
+class DefaultContentType extends Middlewares {
+  @override
+  Future<Response> execute(Request request) async {
+    final response = await innerHandler(request);
+
+    return response
+        .change(headers: {'content-type': 'application/json;charset=utf-8'});
+  }
+}
---
+++ lib/app/core/middlewares/security/security_middleware.dart
@@ -0,0 +1,70 @@
+import 'dart:convert';
+
+import 'package:jaguar_jwt/jaguar_jwt.dart';
+import 'package:shelf/shelf.dart';
+import 'package:stores_api/app/core/helpers/jwt_helpers.dart';
+import 'package:stores_api/app/core/logger/i_logger.dart';
+import 'package:stores_api/app/core/middlewares/middlewares.dart';
+import 'package:stores_api/app/core/middlewares/security/security_skip_url.dart';
+
+class SecurityMiddleware extends Middlewares {
+  final ILogger _log;
+  final skypUrl = <SecuritySkipUrl>[
+    SecuritySkipUrl(url: '/auth/', method: 'POST'),
+    SecuritySkipUrl(url: '/auth/register', method: 'POST'),
+  ];
+
+  SecurityMiddleware({required ILogger log}) : _log = log;
+
+  @override
+  Future<Response> execute(Request request) async {
+    try {
+      if (skypUrl.contains(SecuritySkipUrl(
+          url: '/${request.url.path}', method: request.method))) {
+        return innerHandler(request);
+      }
+
+      final authHeader = request.headers['Authorization'];
+
+      if (authHeader == null || authHeader.isEmpty) {
+        throw JwtException.invalidToken;
+      }
+
+      final authHeaderContent = authHeader.split(' ');
+
+      if (authHeaderContent[0] != 'Bearer') {
+        throw JwtException.invalidToken;
+      }
+
+      final authorizationToken = authHeaderContent[1];
+      final claims = JwtHelpers.getClaim(authorizationToken);
+
+      if (request.url.path != 'auth/refresh') {
+        claims.validate();
+      }
+
+      final claimsMap = claims.toJson();
+
+      final userId = claimsMap['sub'];
+      final supplierId = claimsMap['supplier'];
+
+      if (userId == null) {
+        throw JwtException.invalidToken;
+      }
+
+      final securityHeaders = {
+        'user': userId,
+        'access_token': authorizationToken,
+        'supplier': supplierId != null ? '$supplierId' : null
+      };
+
+      return innerHandler(request.change(headers: securityHeaders));
+    } on JwtException catch (e, s) {
+      _log.error('Erro ao validar token JWT', e, s);
+      return Response.forbidden(jsonEncode({}));
+    } catch (e, s) {
+      _log.error('Internal Server Error', e, s);
+      return Response.forbidden(jsonEncode({}));
+    }
+  }
+}
