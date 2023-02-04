c3a5466721b114e6605e686335610e3bfcd20185
deyvissoneduardo <deyvissonarouche@gmail.com>
2023-02-04 16:49:07 -0300

    init project

---
+++ bin/stores_api.dart
@@ -0,0 +1,5 @@
+import 'package:stores_api/stores_api.dart' as stores_api;
+
+void main(List<String> arguments) {
+  print('Hello world: ${stores_api.calculate()}!');
+}
---
+++ test/stores_api_test.dart
@@ -0,0 +1,8 @@
+import 'package:stores_api/stores_api.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
